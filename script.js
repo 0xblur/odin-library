2ec0759b5dbd4b5e5b4178b26ecfd920c86c798e
cypherHades <xjadezbix@gmx.com>
2024-01-31 16:01:16 -0500

    [MINOR] Whitespaces

--- script.js
+++ script.js
@@ -4,46 +4,43 @@ const myLibrary = [];
 
 // Book object definition.
 class Book {
+	//Constructor
+	constructor(title, author = "", date = "", read = false) {
+		this.title = title;
+		this.author = author;
+		this.date = date;
+		this.read = read;
+	}
+}
 
-  //Constructor
-  constructor(title, author = "", date = "", read = false) {
-    this.title = title;
-    this.author = author;
-    this.date = date;
-    this.read = read;
-  }
-};
-
-Book.prototype.toggleRead = function() {
-  if (this.read) {
-    this.read = false;
-  } else {
-    this.read = true;
-  }
+Book.prototype.toggleRead = function () {
+	if (this.read) {
+		this.read = false;
+	} else {
+		this.read = true;
+	}
 };
 
 // Functions
 function addBookToLibrary() {
-  const newBook = new Book;
-  const form = document.querySelector("form")
-  newBook.title = form.title.value;
-  newBook.author = form.author.value;
-  newBook.date = form.date.value;
-  newBook.read = form.read.checked;
-  //TODO: Add form validation
+	const newBook = new Book();
+	const form = document.querySelector("form");
+	newBook.title = form.title.value;
+	newBook.author = form.author.value;
+	newBook.date = form.date.value;
+	newBook.read = form.read.checked;
+	//TODO: Add form validation
 
-  myLibrary.push(newBook)
-};
-
+	myLibrary.push(newBook);
+}
 
 function displayBooks() {
-  const library = document.querySelector("#library")
-  library.innerHTML = "";
-  myLibrary.forEach((book, i) => {
-
-    const card = document.createElement("div")
-    card.classList.add("book-card")
-    card.innerHTML = `
+	const library = document.querySelector("#library");
+	library.innerHTML = "";
+	myLibrary.forEach((book, i) => {
+		const card = document.createElement("div");
+		card.classList.add("book-card");
+		card.innerHTML = `
     <h2>${book.title}</h2>
     <div class="book-detail-box">
       <ul>
@@ -54,54 +51,56 @@ function displayBooks() {
       </ul>
     </div>
     <div class="book-button-box">
-      <button class="toggle-read book-${i}">${book.read ? "Read" : "Not Read Yet"}</button>
+      <button class="toggle-read book-${i}">${
+				book.read ? "Read" : "Not Read Yet"
+			}</button>
       <button class="delete-book book-${i}">Delete</button>
     </div>
-    `
+    `;
 
-    library.appendChild(card)
-  });
+		library.appendChild(card);
+	});
 
-  // Toggle read/unread status.
-  const toggleReadBtns = document.querySelectorAll("button.toggle-read")
-  toggleReadBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const id = Number(e.target.classList[1].split("-")[1])
-      myLibrary[id].toggleRead()
-      displayBooks()
-    })
-  })
+	// Toggle Read/Unread Status Btn
+	const toggleReadBtns = document.querySelectorAll("button.toggle-read");
+	toggleReadBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			const id = Number(e.target.classList[1].split("-")[1]);
+			myLibrary[id].toggleRead();
+			displayBooks();
+		});
+	});
 
-  // Delete book
-  const deleteBookBtns = document.querySelectorAll("button.delete-book");
-  deleteBookBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const id = Number(e.target.classList[1].split("-")[1])
-      myLibrary.splice(id, 1)
-      displayBooks();
-    })
-  })
+	// Delete Book Btn
+	const deleteBookBtns = document.querySelectorAll("button.delete-book");
+	deleteBookBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			const id = Number(e.target.classList[1].split("-")[1]);
+			myLibrary.splice(id, 1);
+			displayBooks();
+		});
+	});
 }
 
 // Event Listeners
-// Open Dialog
-const openDialogBtn = document.getElementById("open-dialog")
-openDialogBtn.addEventListener("click", () => dialog.showModal())
+// Open Dialog Btn
+const openDialogBtn = document.getElementById("open-dialog");
+openDialogBtn.addEventListener("click", () => dialog.showModal());
 
-// Close Dialog
-const closeDialogBtn = document.querySelector("button#close-dialog")
+// Close Dialog Btn
+const closeDialogBtn = document.querySelector("button#close-dialog");
 closeDialogBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  dialog.close()
+	e.preventDefault();
+	dialog.close();
 });
 
-// Add book button.
+// Add Book Btn
 //TODO: Add keyboard shortcuts.
 const addBookBtn = document.querySelector("button#add-book");
 addBookBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-  form.reset();
-  dialog.close();
-  displayBooks();
+	e.preventDefault();
+	addBookToLibrary();
+	form.reset();
+	dialog.close();
+	displayBooks();
 });
